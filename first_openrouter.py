35692856e879f5bfb396d12bb323e24b8a2bae25
Andrej Babkov <andrekb@volny.cz>
2026-08-08 17:29:28 +0200

    add openrouter ai with error handling and variables for response

--- first_openrouter.py
+++ first_openrouter.py
@@ -1,22 +1,26 @@
-from mistralai.client import Mistral, errors
+from openai import OpenAI, OpenAIError
+from dotenv import load_dotenv
 from time import perf_counter
 import os
 import sys
-from dotenv import load_dotenv
 
 
 load_dotenv()
 
-API_KEY = os.environ["MISTRAL_API_KEY"]
-MODEL = "mistral-small-latest"
+BASE_URL = "https://openrouter.ai/api/v1"
+API_KEY = os.environ["OPENROUTER_API_KEY"]
+MODEL = "openai/gpt-oss-20b:free"
 PROMPT = "Hi! Briefly explain what FastAPI is."
 
-client = Mistral(api_key=API_KEY)
+client = OpenAI(
+    base_url=BASE_URL,
+    api_key=API_KEY,
+)
 
 start_time = perf_counter()
 
 try:
-    response = client.chat.complete(
+    response = client.chat.completions.create(
         model=MODEL,
         messages=[
             {"role": "user",
@@ -26,8 +30,8 @@ try:
         temperature=0.2,
         max_tokens=300,
     )
-except errors.mistralerror as e:
-    print(f"Mistral API error: {e}")
+except OpenAIError as e:
+    print(f"OpenRouter API error: {e}")
     sys.exit(1)
 
 response_time = perf_counter() - start_time
